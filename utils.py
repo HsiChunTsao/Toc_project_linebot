380bf9b23c07c89a05629f31381427aac34e4e44
HST <sonic55567@gmail.com>
2019-12-05 10:43:40 +0800

    change token

--- utils.py
+++ utils.py
@@ -4,11 +4,11 @@ from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
-channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+#channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 
 def send_text_message(reply_token, text):
-    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api = LineBotApi('pFaw0u18MzAna8ZIF6Eby7Hm6mEHkLpamUocVb/+iiqv7UYRN/GDcW8Z5s2SllDlPXxMFMNjetkk1glMdW+qoVIItHrvB9926qSGWoM514NXPMoultRhfrYctZ9x7TW5hjKD76EUJ4NgenCj68liYQdB04t89/1O/w1cDnyilFU=')
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
